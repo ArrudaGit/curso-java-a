02cddd9382dbb597cc2904970949ea46a82330a5
Person Arruda <41989037+ArrudaGit@users.noreply.github.com>
2025-09-29 10:13:36 -0300

    adicionado novamente.

--- exercicios/grupo1/Exercicio5.java
+++ exercicios/grupo1/Exercicio5.java
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-// Source code is decompiled from a .class file using FernFlower decompiler (from Intellij IDEA).
-import java.util.Scanner;
-
-public class Exercicio5 {
-   public Exercicio5() {
-   }
-
-   public static void main(String[] var0) {
-      Scanner var1 = new Scanner(System.in);
-      System.out.print("Calcule a area de um quadrado. Digite o valor de um lado: ");
-      double var2 = var1.nextDouble();
-      double var4 = var2 * var2;
-      System.out.printf("A area do quadrado é %.2f%n", var4);
-      var1.close();
-   }
-=======
 /*
  * Objetivo: Calcule a área de um quadrado. Como calcular: Area = lado \* lado
  * @creator: Rodrigo Fischer (programacao.mentoria)
  */
-package exercicios.grupo1;
+// package exercicios.grupo1;
 
 import java.util.Scanner;
 
@@ -35,5 +18,5 @@ public class Exercicio5 {
         System.out.printf("A área do quadrado que tem a medida de seu lado = %d é: %.2f", lado, areaQuadrado);
         scanner.close();
     }
->>>>>>> 42c3f5a92abd718ccda1fffa40064e10c79d8ba9
+
 }
