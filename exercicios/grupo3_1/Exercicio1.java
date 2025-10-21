9eea4c20ee980f64fd1f7ecbb1f99cbb5716d1a3
Person Arruda <41989037+ArrudaGit@users.noreply.github.com>
2025-10-21 17:14:22 -0300

    resolvido.

---
+++ exercicios/grupo3_1/Exercicio1.java
@@ -0,0 +1,32 @@
+/**
+ * Contador Crescente: 
+ * Crie um algoritmo que imprima na tela todos os números inteiros de 1 a 100.
+ */
+
+package exercicios.grupo3_1;
+
+public class Exercicio1 {
+    public static void main(String[] args) 
+    {
+        for (int numero = 1; numero <= 100; numero++)
+        {
+            System.out.print(numero + " FOR" + " ");
+        }
+   
+    }
+}
+
+// LOOP COM WHILE
+// package exercicios.grupo3_1;
+
+// public class Exercicio1 {
+//     public static void main(String[] args) {
+//     int numero = 1; //contador
+    
+//     while (numero <= 100) 
+//     {
+//         System.out.print(numero + " ");
+//         numero++;    
+//     }
+// } 
+// }
---
+++ exercicios/grupo3_1/Exercicio2.java
@@ -0,0 +1,33 @@
+/**
+ 2 3.1 Contador Decrescente: Desenvolva um programa que imprima na tela todos os números
+inteiros de 50 a 1, em ordem decrescente.
+ **/
+
+package exercicios.grupo3_1;
+
+public class Exercicio2 {
+    public static void main(String[] args) 
+    {
+        for (int numero = 50; numero >= 0; numero--)
+        {
+            System.out.print(numero + " ");
+        }
+   
+    }
+} 
+
+// loop com WHILE
+
+// package exercicios.grupo3_1;
+
+// public class Exercicio2 {
+//     public static void main(String[] args) {
+//     int numero = 50;
+    
+//     while (numero >= 1) 
+//         {
+//         System.out.print(numero + " ");
+//         numero--;
+//         }
+//     }
+// } 
