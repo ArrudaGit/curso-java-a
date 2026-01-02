293075ae7eaca190494531a878d2acfcd5713f89
Person Arruda <41989037+ArrudaGit@users.noreply.github.com>
2026-01-02 16:11:09 -0300

    .

--- exercicios/grupo2/Exercicio6.java
+++ exercicios/grupo2/Exercicio6.java
@@ -1,35 +1,60 @@
-/**
- * Pergunte a idade de uma pessoa. Diga se essa pessoa é uma criança (até 12 anos), um
-  adolescente (13 a 17 anos), uma adulta (18 a 59 anos) ou idosa (60 anos ou mais)
-  * @creator: Rodrigo Fischer (programacao.mentoria)
- */
-package exercicios.grupo2;
 
-import java.util.*;
+
+// /**
+//  * Pergunte a idade de uma pessoa. Diga se essa pessoa é uma criança (até 12 anos), um
+//   adolescente (13 a 17 anos), uma adulta (18 a 59 anos) ou idosa (60 anos ou mais)
+//   * @creator: Rodrigo Fischer (programacao.mentoria)
+//  */
+ package exercicios.grupo2;
+
+import java.util.Scanner;
 
 public class Exercicio6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Digite uma idade: ");
+        System.out.print("Digite a idade: ");
         int idade = scanner.nextInt();
+        String categoria;
 
-        if (idade > 0 && idade <= 12) {
-            System.out.println("Criança");
-        }
-        else if(idade >= 13 && idade <= 17){
-            System.out.println("Adolescente");
-        }
-        else if (idade >= 18 && idade <= 59) {
-            System.out.println("Adulta");
-        }
-        else if(idade >= 60) {
-            System.out.println("Idosa");
-        }
-        else {
-            System.out.println("Idade inválida");
-        }
+        // Lógica em "escadinha"
+        if (idade < 0)        categoria = "Inválida";
+        else if (idade <= 12) categoria = "Criança";
+        else if (idade <= 17) categoria = "Adolescente";
+        else if (idade <= 59) categoria = "Adulta";
+        else                  categoria = "Idosa";
 
+        System.out.println("Resultado: " + categoria);
+        
         scanner.close();
     }
 }
+
+// public class Exercicio6 {
+//     public static void main(String[] args) {
+//         Scanner scanner = new Scanner(System.in);
+
+//         System.out.print("Digite uma idade: ");
+//         int idade = scanner.nextInt();
+
+//         if (idade > 0 && idade <= 12) {
+//             System.out.println("Criança");
+//         }
+//         else if(idade >= 13 && idade <= 17){
+//             System.out.println("Adolescente");
+//         }
+//         else if (idade >= 18 && idade <= 59) {
+//             System.out.println("Adulta");
+//         }
+//         else if(idade >= 60) {
+//             System.out.println("Idosa");
+//         }
+//         else {
+//             System.out.println("Idade inválida");
+//         }
+
+//         scanner.close();
+//     }
+// }
+
+
